91798ae528a40ab42f047641c62cb8944162bf30
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-30 11:46:29 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# learn_react_30092019
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <MainContent />
+      <Footer />
+    </React.Fragment>
+  );
+}
+export default App;
